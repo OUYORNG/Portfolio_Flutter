c3949f3e804d079c87542dbafd1199df81051827
Your Name <you@example.com>
2025-03-19 15:44:55 +0700

    Upload Flutter

---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:open_file/open_file.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(textTheme: GoogleFonts.robotoTextTheme()),
+      home: PortfolioScreen(),
+    );
+  }
+}
+
+class PortfolioScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        title: Row(
+          children: [
+            Image.network(
+              'https://gic.itc.edu.kh/img/logo.png', // Replace with your image URL
+              height: 40, // Adjust the height as needed
+              width: 40, // Adjust the width as needed
+            ),
+            SizedBox(width: 8), // Add spacing between the image and the text
+            Text("GIC-25th", style: GoogleFonts.roboto(fontSize: 20)),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu, color: Colors.white, size: 35),
+            onPressed: () {},
+          ),
+        ],
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 400,
+              height: 400,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/ORNG.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            // Welcome Text
+            Text(
+              "WELCOME TO MY PORTFOLIO",
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            SizedBox(height: 10),
+
+            // Name and Title
+            Text(
+              "HI I'M",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              "OUYORNG KHEANG",
+              style: GoogleFonts.roboto(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+            Text(
+              "4th YEAR IT STUDENT",
+              style: GoogleFonts.roboto(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 20),
+            // Description
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Text(
+                "A very curious and passionate to learn new things with flexible and easy-going personality. I have a strong commitment to learn multiple programming languages and tools. I would prefer my leisure time to keep up with the upcomming technology and improves my skills.",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.roboto(fontSize: 16, color: Colors.grey),
+              ),
+            ),
+            SizedBox(height: 30),
+
+            // Hire Me Button
+            TextButton(
+              onPressed: () async {
+                const telegramUrl =
+                    'tg://resolve?domain=OUYORNG'; // Replace 'username' with the Telegram username
+                if (await canLaunchUrl(Uri.parse(telegramUrl))) {
+                  await launchUrl(Uri.parse(telegramUrl));
+                } else {
+                  // Fallback to the web version if the Telegram app is not installed
+                  const fallbackUrl =
+                      'https://t.me/OUYORNG'; // Replace 'username' with the Telegram username
+                  if (await canLaunchUrl(Uri.parse(fallbackUrl))) {
+                    await launchUrl(Uri.parse(fallbackUrl));
+                  } else {
+                    throw 'Could not launch Telegram';
+                  }
+                }
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 25,
+                ),
+                child: const Text(
+                  'Hire Me',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            // Download CV Button
+            OutlinedButton(
+              onPressed: () async {
+                final filePath = 'assets/cv.pdf'; // Path to your local CV file
+                try {
+                  await OpenFile.open(filePath);
+                } catch (e) {
+                  print('Error opening file: $e');
+                }
+              },
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: Colors.blue),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Download CV",
+                    style: TextStyle(fontSize: 18, color: Colors.blue),
+                  ),
+                  SizedBox(width: 5),
+                  Icon(Icons.download, color: Colors.blue),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
